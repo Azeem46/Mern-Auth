28b06f78d488276bdcb9b4c4cfa1fea6ea7905ce
Azeem46 <shaikhazeem4646@gmail.com>
2024-08-07 18:51:35 +0530

    added link

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 
-const USERS_URL = '/api/users';
+const USERS_URL = 'https://mern-auth-0eyb.onrender.com/api/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
