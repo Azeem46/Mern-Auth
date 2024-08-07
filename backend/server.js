5481a9e342294ba77263a2894c3537c5b5ae359f
Azeem46 <shaikhazeem4646@gmail.com>
2024-08-07 19:00:33 +0530

    added import

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
