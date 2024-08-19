a3b139a1018f2e85d1746012730b195893d552bb
Azeem46 <shaikhazeem4646@gmail.com>
2024-08-19 11:46:21 +0530

    added vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-auth-0eyb.onrender.com',
         changeOrigin: true
       }
     }
