bdef56eaa00d5026cbc5c5b445a151310f99e554
Azeem46 <shaikhazeem4646@gmail.com>
2024-08-19 11:25:53 +0530

    added cron job and corrected api

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
-
+import cron from 'node-cron';
 
 connectDB();
 
@@ -25,7 +25,7 @@ const corsOptions = {
     'http://localhost:3000', // Development URL
     'https://mern-auth-1-5zyo.onrender.com', // Production URL
     'https://mern-auth-0eyb.onrender.com',
-    'https://mern-auth-hgxd.vercel.app'
+    'https://mern-auth-hgxd.vercel.app',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
@@ -37,18 +37,23 @@ app.use(cors(corsOptions));
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-    app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
 } else {
-    app.get('/', (req, res) => res.send('Server is running'));
+  app.get('/', (req, res) => res.send('Server is running'));
 }
+
+// Example cron job that runs every 10 minutes
+cron.schedule('*/5 * * * * *', () => {
+  // console.log('server is healthy');
+  // Add your task here, such as updating records, clearing cache, etc.
+});
 
 // Error handling middleware
 app.use(notFound);
@@ -56,6 +61,6 @@ app.use(errorHandler);
 
 // Start the server
 const port = process.env.PORT || 5000;
-app.listen(port, () => 
-    console.log(`Server running on port ${port}`)
+app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
 );
